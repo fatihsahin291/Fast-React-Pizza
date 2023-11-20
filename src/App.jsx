d5825006653cb5189f42776a7b9aaeab9c671f23
fatihsahin291 <musabsahin291@gmail.com>
2023-11-20 20:42:41 +0300

    implementing react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,43 @@
+import {
+	RouterProvider,
+	createBrowserRouter,
+} from "react-router-dom";
+
+import Home from "./UI/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Home />,
+	},
+	{
+		path: "/menu",
+		element: <Menu />,
+	},
+	{
+		path: "/cart",
+		element: <Cart />,
+	},
+	{
+		path: "/order/new",
+		element: <CreateOrder />,
+	},
+	{
+		path: "/order/:orderId",
+		element: <Order />,
+	},
+]);
+
 function App() {
-	const x = 1;
-	return <div>drgsfgdfg</div>;
+	return (
+		<RouterProvider
+			router={router}
+		></RouterProvider>
+	);
 }
 
 export default App;
