747d6774f627ef1eccd3855437c0cddde7d5785c
fatihsahin291 <musabsahin291@gmail.com>
2023-11-21 13:24:18 +0300

    fetching pizza menu with react router

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import {
 } from "react-router-dom";
 
 import Home from "./UI/Home";
-import Menu from "./features/menu/Menu";
+import Menu, {
+	loader as menuLoader,
+} from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
@@ -21,6 +23,7 @@ const router = createBrowserRouter([
 			{
 				path: "/menu",
 				element: <Menu />,
+				loader: menuLoader,
 			},
 			{
 				path: "/cart",
