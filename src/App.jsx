e0a85694338dc6c90f63f9f0426ece47ab51c479
fatihsahin291 <musabsahin291@gmail.com>
2023-12-01 16:47:54 +0300

    implementing createAsyncThunk and useFetcher

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,53 @@
-import {
-	RouterProvider,
-	createBrowserRouter,
-} from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import Home from "./UI/Home";
-import Menu, {
-	loader as menuLoader,
-} from "./features/menu/Menu";
-import Cart from "./features/cart/Cart";
+import Home from './UI/Home';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
+import Cart from './features/cart/Cart';
 import CreateOrder, {
-	action as createOrderAction,
-} from "./features/order/CreateOrder";
-import Order, {
-	loader as orderLoader,
-} from "./features/order/Order";
-import AppLayout from "./UI/AppLayout";
-import Error from "./UI/Error";
+  action as createOrderAction,
+} from './features/order/CreateOrder';
+import { action as updateOrderAction } from './features/order/UpdateOrder';
+import Order, { loader as orderLoader } from './features/order/Order';
+import AppLayout from './UI/AppLayout';
+import Error from './UI/Error';
 
 const router = createBrowserRouter([
-	{
-		element: <AppLayout />,
-		errorElement: <Error />,
-		children: [
-			{
-				path: "/",
-				element: <Home />,
-			},
-			{
-				path: "/menu",
-				element: <Menu />,
-				loader: menuLoader,
-				errorElement: <Error />,
-			},
-			{
-				path: "/cart",
-				element: <Cart />,
-			},
-			{
-				path: "/order/new",
-				element: <CreateOrder />,
-				action: createOrderAction,
-			},
-			{
-				path: "/order/:orderId",
-				element: <Order />,
-				loader: orderLoader,
-				errorElement: <Error />,
-			},
-		],
-	},
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/menu',
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+      {
+        path: '/order/new',
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
+      {
+        path: '/order/:orderId',
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+        action: updateOrderAction,
+      },
+    ],
+  },
 ]);
 
 function App() {
-	return (
-		<RouterProvider
-			router={router}
-		></RouterProvider>
-	);
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,8 +2,13 @@ import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../UI/Button';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart, getCart, getTotalCartPrice } from '../cart/cartSlice';
+import EmptyCart from '../cart/EmptyCart';
 useState;
+import store from '../../store';
+import { formatCurrency } from '../../utils/helpers';
+import { fetchAddress } from '../user/userSlice';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -11,37 +16,27 @@ const isValidPhone = (str) =>
     str,
   );
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
-
 function CreateOrder() {
-  const userName = useSelector((state) => state.user.userName);
+  const dispatch = useDispatch();
+  const [withPriority, setWithPriority] = useState(false);
+  const {
+    userName,
+    status: addressStatus,
+    position,
+    address,
+    error: errorAddress,
+  } = useSelector((state) => state.user);
+  const isLoadingAddress = addressStatus === 'loading';
+
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
   const formErrors = useActionData();
-  // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
+  const cart = useSelector(getCart);
+  const totalCartPrice = useSelector(getTotalCartPrice);
+  const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
+  const totalPrice = totalCartPrice + priorityPrice;
+
+  if (!cart.length) return <EmptyCart />;
 
   return (
     <div className="px-6 py-6">
@@ -73,16 +68,37 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div className="mb-5 flex flex-col  gap-2 sm:flex-row sm:items-center">
+        <div className="relative mb-5 flex  flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
           <div className="grow">
             <input
               className="input w-full"
               type="text"
               name="address"
+              disabled={isLoadingAddress}
+              defaultValue={address}
               required
             />
+            {addressStatus === 'error' && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {errorAddress}
+              </p>
+            )}
           </div>
+          {address ? null : (
+            <span className="absolute right-[3px] top-[3px] z-50 md:right-[5px] md:top-[5px]">
+              <Button
+                disabled={isLoadingAddress}
+                type="small"
+                onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(fetchAddress());
+                }}
+              >
+                Get Position
+              </Button>
+            </span>
+          )}
         </div>
 
         <div className="mb-12 flex items-center gap-5">
@@ -91,8 +107,8 @@ function CreateOrder() {
             name="priority"
             id="priority"
             className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
+            value={withPriority}
+            onChange={(e) => setWithPriority(e.target.checked)}
           />
           <label className="font-medium" htmlFor="priority">
             Want to yo give your order priority?
@@ -101,8 +117,19 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button type="primary" disabled={isSubmitting}>
-            {isSubmitting ? 'Placing Order...' : 'Order now'}
+          <input
+            type="hidden"
+            name="position"
+            value={
+              position.longitude && position.latitude
+                ? `${position.latitude},${position.longitude}`
+                : ''
+            }
+          />
+          <Button type="primary" disabled={isSubmitting || isLoadingAddress}>
+            {isSubmitting
+              ? 'Placing Order...'
+              : `Order now from ${formatCurrency(totalPrice)}`}
           </Button>
         </div>
       </Form>
@@ -114,13 +141,13 @@ export async function action({ request }) {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
 
-  console.log(data);
-
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === 'on',
+    priority: data.priority === 'true',
   };
+
+  console.log('order', order);
 
   const errors = {};
   if (!isValidPhone(order.phone)) errors.phone = 'Invalid phone number';
@@ -129,6 +156,9 @@ export async function action({ request }) {
 
   // If there are no errors, create the order and redirect to it
   const newOrder = await createOrder(order);
+
+  // Clear the cart
+  store.dispatch(clearCart());
 
   return redirect(`/order/${newOrder.id}`);
 }
